5a653f5eee3a664a28aa074c5e9c1182cef596ff
LindsayLew <lmschwehr@gmail.com>
2022-07-30 20:49:00 -0700

    fixing model index

--- models/index.js
+++ models/index.js
@@ -4,17 +4,21 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+// const Products = sequelize.define('Products', )
 // Products belongsTo Category
 Product.belongsTo(Category);
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'CategoryProductId'
+});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsTo(Tag);
+Product.belongsTo(Tag, { through: 'ProductTag'});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product);
+Tag.belongsToMany(Product, { through: 'ProductTag'});
 
 module.exports = {
   Product,
